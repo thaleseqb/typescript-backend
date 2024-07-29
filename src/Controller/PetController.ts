740b3635e5e93154a00395ef0bd827f7a7793c7d
thaleseqb <thalesquadros2@gmail.com>
2024-07-29 11:01:15 -0300

    create a file for typing variables and add some code refactors

--- src/Controller/PetController.ts
+++ src/Controller/PetController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
+import Pet from "../types/Pet";
 
-let petList = [];
+let petList: Array<Pet> = [];
 
 export default class PetController {
     criaPet(req:Request, res: Response) {
-        const newPet = req.body;
+        const {id, 
+            adotado, 
+            especie, 
+            idade, 
+            nome} = <Pet>req.body;
+        const newPet = {id, adotado, especie, idade, nome}
         petList.push(newPet);
         return res.status(201).json(newPet);
     }
---
+++ src/types/Pet.ts
@@ -0,0 +1,7 @@
+export default interface Pet {
+    id:number;
+    nome:string;
+    especie:string;
+    idade:number;
+    adotado:boolean
+}
